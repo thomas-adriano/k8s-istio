b018f7767813f99a4460043b722e9551fd94d67b
Thomas O. Adriano <thomas.o.adriano@gmail.com>
2020-11-02 14:32:08 -0300

    Added wait for ingressgatewway service; Logs improved;

--- istio-operator.sh
+++ istio-operator.sh
@@ -4,19 +4,18 @@ ISTIO_VERSION=1.7.3
 
 curl -L "https://github.com/istio/istio/releases/download/$ISTIO_VERSION/istioctl-$ISTIO_VERSION-linux-amd64.tar.gz" | tar xz
 
-./istioctl install
+./istioctl install -y
 
 kubectl create ns istio-system
 kubectl label namespace istio-system istio-injection=enabled
 kubectl label namespace default istio-injection=enabled
 
 kubectl apply -f istio-operator.yml
-while ! kubectl wait --for=condition=available --timeout=600s deployment/istio-ingressgateway -n istio-system; do sleep 1; done
+while ! kubectl wait --for=condition=available --timeout=600s service/istio-ingressgateway -n istio-system; do sleep 1; done
 # Addons
 export INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-# Used by istio-grafana-ingress.yml
 export INGRESS_DOMAIN=${INGRESS_HOST}.nip.io
-echo "Ingress domain created"
+echo "Ingress domain created: ${INGRESS_DOMAIN}"
 
 ## Grafana HTTP
 kubectl replace --force -f https://raw.githubusercontent.com/istio/istio/release-1.7/samples/addons/grafana.yaml
