9971717f3e90857e35853367405c399840c4880b
Thomas O. Adriano <thomas.o.adriano@gmail.com>
2020-11-02 11:58:58 -0300

    changed to istioctl install

--- istio-operator.sh
+++ istio-operator.sh
@@ -4,7 +4,7 @@ ISTIO_VERSION=1.7.3
 
 curl -L "https://github.com/istio/istio/releases/download/$ISTIO_VERSION/istioctl-$ISTIO_VERSION-linux-amd64.tar.gz" | tar xz
 
-./istioctl operator init
+./istioctl install
 
 kubectl create ns istio-system
 kubectl label namespace istio-system istio-injection=enabled
@@ -22,6 +22,6 @@ echo "Ingress domain created"
 kubectl replace --force -f https://raw.githubusercontent.com/istio/istio/release-1.7/samples/addons/grafana.yaml
 kubectl replace --force -f istio-grafana-ingress.yml
 while ! kubectl wait --for=condition=available --timeout=600s deployment/grafana -n istio-system; do sleep 1; done
-echo "-----> You can access Grafana at http://${INGRESS_HOST}.nip.io"
+echo "-----> You can access Grafana at http://grafana.${INGRESS_DOMAIN}"
 
 echo "-----> Script finished successfully."
