bdf717bc5dbdce70ffe3bd9b23c19c2bbbee8e7f
Thomas O. Adriano <thomas.o.adriano@gmail.com>
2020-11-02 11:33:16 -0300

    added logs and instio-injection to istio-ssystem

--- istio-operator.sh
+++ istio-operator.sh
@@ -7,6 +7,7 @@ curl -L "https://github.com/istio/istio/releases/download/$ISTIO_VERSION/istioct
 ./istioctl operator init
 
 kubectl create ns istio-system
+kubectl label namespace istio-system istio-injection=enabled
 kubectl label namespace default istio-injection=enabled
 
 kubectl apply -f istio-operator.yml
@@ -15,12 +16,12 @@ kubectl apply -f istio-operator.yml
 export INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 # Used by istio-grafana-ingress.yml
 export INGRESS_DOMAIN=${INGRESS_HOST}.nip.io
+echo "Ingress domain created"
 
 ## Grafana HTTP
 kubectl replace --force -f https://raw.githubusercontent.com/istio/istio/release-1.7/samples/addons/grafana.yaml
 kubectl replace --force -f istio-grafana-ingress.yml
 while ! kubectl wait --for=condition=available --timeout=600s deployment/grafana -n istio-system; do sleep 1; done
-
 echo "-----> You can access Grafana at http://${INGRESS_HOST}.nip.io"
 
 echo "-----> Script finished successfully."
